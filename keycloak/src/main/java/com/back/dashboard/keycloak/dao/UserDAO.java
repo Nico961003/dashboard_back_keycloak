64a96b41bcd4917ca693b382ab461d003a636f28
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-06-22 17:59:04 -0500

    first configurations

--- keycloak/src/main/java/com/back/dashboard/keycloak/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.back.dashboard.keycloak.dao;
-import org.springframework.data.repository.CrudRepository;
-import com.back.dashboard.keycloak.model.User;
-
-public interface UserDAO extends CrudRepository<User, Long> {
-
-}
